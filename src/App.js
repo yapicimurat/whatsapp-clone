5b6ac482af922f53eb0c3b0dde00bbfbaa05388a
yapicimurat <muratyapiicii@gmail.com>
2022-04-19 16:01:27 +0300

    router package

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import "./assets/css/style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as icons from "@fortawesome/free-solid-svg-icons";
